fa650e429d28ea3e552053aacc8c307ac54d53ef
Wrenard Guevara <guevarawrenard@gmail.com>
2023-04-03 01:10:16 -0700

    fixed repeating operator

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Buttonstyle from "./component/Buttonstyle";
 
 export default function App() {
   const [Output, setOutput] = useState("");
+  const [operator, setOperator] = useState("");
+  const [preValue, setPreValue] = useState("");
+  const [display, setDisplay] = useState("0");
 
-  function handleClick(value: number | string) {
+  function handleClick(value: string): void {
     switch (value) {
       case "C":
-        setOutput("");
+        setOutput("0");
+        setOperator("");
+        setPreValue("");
         break;
 
       case "+-":
         setOutput((-1 * parseInt(Output)).toString());
         break;
-
       case ".":
         if (!Output.includes(".")) {
           setOutput(Output + ".");
@@ -24,47 +28,55 @@ export default function App() {
         setOutput((Number(Output) / 100).toString());
         break;
 
-      default:
-        setOutput(Output + value);
-        break;
-
       case "+":
-        if (!Output.includes("+")) {
-          setOutput(Output + value.toString());
-        } else {
-          null;
-        }
-        break;
-
       case "-":
-        if (!Output.includes("-")) {
-          setOutput(Output + value.toString());
-        } else {
-          null;
-        }
-        break;
-
-      case "x":
-        if (!Output.includes("x")) {
-          setOutput(Output + value.toString());
-        } else {
-          null;
-        }
-        break;
-
+      case "*":
       case "/":
-        if (!Output.includes("/")) {
-          setOutput(Output + value.toString());
-        } else {
-          null;
-        }
+        operatorClick(value);
         break;
-
       case "=":
-        setOutput(eval(Output).toString());
+        equal();
+        break;
+      default:
+        Output !== "0" ? setOutput(Output + value) : setOutput(value);
         break;
     }
   }
+
+  const operatorClick = (buttonValue: string) => {
+    setOperator(buttonValue);
+
+    if (preValue !== "") {
+    } else {
+      setPreValue(Output);
+      setOutput(" ");
+    }
+  };
+
+  const equal = () => {
+    let equals = 0;
+    const prevalues = parseFloat(preValue);
+    const outputs = parseFloat(Output);
+
+    if (operator === "+") {
+      equals = prevalues + outputs;
+    } else if (operator === "-") {
+      equals = prevalues - outputs;
+    } else if (operator === "/") {
+      equals = prevalues / outputs;
+    } else if (operator === "*") {
+      equals = prevalues * outputs;
+    }
+
+    setOperator("");
+    setOutput("");
+    setPreValue(equals.toString());
+  };
+
+  useEffect(() => {
+    setDisplay(Output);
+  }, [Output]);
+
   return (
     <div className="bg-yellow-400 h-screen flex flex-row justify-center place-items-center">
       <div className="bg-gray-800 p-1 flex flex-col items-center gap-1 rounded-md">
@@ -72,7 +84,7 @@ export default function App() {
           className="bg-gray-700 h-12 w-52 flex flex-row text-right text-white text-3xl font-semibold rounded-md"
           disabled
           type="text"
-          value={Output}
+          value={display ? display : preValue}
           placeholder="0"
         />
         <div className="grid grid-cols-4 grid-rows-5 gap-1 w-52">
